82a353c046130a8326feaf3da6f1999b703e1823
masaruf <masaru.fujiii@gmail.com>
2014-12-05 05:55:49 +0900

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirtApp::Application.config.secret_key_base = '858bad4fb5def33311bbca67f9df900c346d56d02282de2c6ac258fbaa291d4124efdffbd7cf7d2b9dd95ac718c98fbda82070e3d253e3f7a29c3eb10175f621'
